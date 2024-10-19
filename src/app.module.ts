2d174f110ea836d6717b8593a782423dca9a7e94
duongbui <duongbuibd@gmail.com>
2024-10-19 16:33:15 +0700

    feat: Add CreateUserDto, UpdateUserDto, and User entity
    - Add CreateUserDto class to handle creating new users.
    - Add UpdateUserDto class to handle updating existing users.
    - Add User entity with name and age properties.
    
    Closes #123

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
